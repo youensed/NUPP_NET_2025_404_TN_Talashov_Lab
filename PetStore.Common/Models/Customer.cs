3096cdf6c004cd55d298d7609b90a7207e51b83c
youensed <bogdan.talashov.2@gmail.com>
2025-10-23 23:42:11 +0300

    add lab1

---
+++ PetStore.Common/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PetStore.Common.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // статичне поле
+        public static int TotalCustomers;
+
+        // статичний конструктор
+        static Customer()
+        {
+            TotalCustomers = 0;
+        }
+
+        // конструктор
+        public Customer(string name, int age)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Age = age;
+            TotalCustomers++;
+        }
+
+        // метод
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name}, {Age} років (Загальна кількість клієнтів: {TotalCustomers})");
+        }
+
+        // статичний метод
+        public static void ShowTotalCustomers()
+        {
+            Console.WriteLine($"Зареєстровано клієнтів: {TotalCustomers}");
+        }
+    }
+}
---
+++ PetStore.Common/Models/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PetStore.Common.Models
+{
+    // клас-нащадок
+    public class Dog : Pet
+    {
+        public string Breed { get; set; }
+        public bool IsTrained { get; set; }
+
+        // делегат і подія
+        public delegate void BarkedHandler(Dog dog);
+        public event BarkedHandler? OnBark;
+
+        // конструктор
+        public Dog(string name, int age, string breed, bool isTrained)
+            : base(name, age)
+        {
+            Breed = breed;
+            IsTrained = isTrained;
+        }
+
+        // метод
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} гавкає");
+            OnBark?.Invoke(this);
+        }
+
+        // перевизначений метод
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} гавкає гучно!");
+        }
+    }
+}
